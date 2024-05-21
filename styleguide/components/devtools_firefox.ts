71b2bdfbde7e0bf64ae33b409397d57f138875af
VALLET Jonathan <jonathan.vallet@arneo.io>
2024-05-21 18:03:44 +0200

    Ajout de copyright manquants + fix icone devtools firefox

--- styleguide/components/devtools_firefox.ts
+++ styleguide/components/devtools_firefox.ts
@@ -1,4 +1,4 @@
-browser.devtools.panels.create('RGAA Checker', 'images/icon64.png', 'rgaa-checker-panel.html').then((panel) => {
+browser.devtools.panels.create('RGAA Checker', '/images/icon48.png', 'rgaa-checker-panel.html').then((panel) => {
   panel.onShown.addListener((panelWindow) => {
     browser.runtime.sendMessage({ action: "devtools_panelShown" });
   });
